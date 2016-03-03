ab9dab89c8a950dd2e410ec81fe30f7195b41c12
inftel06 <inftel06@localhost>
2016-03-03 09:26:57 +0100

    Login, avanzado

--- ManageMe-war/web/resources/js/functions.js
+++ ManageMe-war/web/resources/js/functions.js
@@ -16,6 +16,7 @@ function onSignIn(googleUser) {
         // The ID token you need to pass to your backend:
         var id_token = googleUser.getAuthResponse().id_token;
         console.log("ID Token: " + id_token);
+        
             
             window.location.replace("faces/pages/chatPage.xhtml");
       };
@@ -23,9 +24,9 @@ function onSignIn(googleUser) {
       
   function signOut() {
       alert("fernandin");
-    var auth2 = gapi.auth2.getAuthInstance();
-    
-    auth2.signOut().then(function () {
+    var aouth2 = gapi.auth2.getAuthInstance();
+   
+    aouth2.signOut().then(function () {
       console.log('User signed out.');
       alert("Hasta luego cabrones0");
     });
