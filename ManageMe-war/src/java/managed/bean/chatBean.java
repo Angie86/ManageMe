4f1fc2d1d42ed29d8fc7b5199493470d5ab9a55a
inftel06 <inftel06@localhost>
2016-03-03 09:20:10 +0100

    Login, avanzado

---
+++ ManageMe-war/src/java/managed/bean/chatBean.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managed.bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author inftel08
+ */
+@Named(value = "chatBean")
+@RequestScoped
+public class chatBean {
+
+    /**
+     * Creates a new instance of chatBean
+     */
+    public chatBean() {
+    }
+    
+    public String doShowChat(){
+    
+        return ("chatPage");
+    }
+    
+}
---
+++ ManageMe-war/src/java/managed/bean/historicBean.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managed.bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author inftel08
+ */
+@Named(value = "historicBean")
+@RequestScoped
+public class historicBean {
+
+    /**
+     * Creates a new instance of historicBean
+     */
+    public historicBean() {
+    }
+    
+    public String doShowHistoric(){
+    
+        return ("historicPage");
+    }
+    
+    
+}
